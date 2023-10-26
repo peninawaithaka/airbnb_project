6e9304ec9b494304ab7f414a2ad72dccfd756eb7
Penina Waithaka <peninawaithaka5@gmail.com>
2023-10-26 18:39:06 +0300

    Added scd raw losting snapshot

---
+++ snapshots/scd_raw_listings.sql
@@ -0,0 +1,17 @@
+-- slowly changing dimension type 2 - keeps all historical data
+
+{% snapshot scd_raw_listings %}
+
+{{
+    config(
+        target_schema = 'dev',
+        unique_key = 'id',
+        strategy = 'timestamp',
+        updated_at = 'updated_at',
+        invalidate_hard_deletes = True
+    )
+}}
+
+select * from {{ source('airbnb', 'listings')}}
+
+{% endsnapshot %}
